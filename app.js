846cedb133f51cda0829fc255c2d5f469d44f18f
yashladekar <yashladekar002@gmail.com>
2023-02-07 17:53:44 +0530

    game of life with heatmap visualization

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express();
+
+
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+    res.send();                    
+})
+
+
+app.listen(5001, () => {
+    console.log('gol started on server http://localhost:5001')
+})
